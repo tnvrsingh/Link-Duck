9a73ee55cc8c9de3d2656eef46662afd46aacdf1
Tanvir Singh <tanvir.singh96@live.com>
2017-04-16 04:56:47 +0530

    added new url route and DB connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var shortid = require('shortid');
+var validUrl = require('valid-url');
+
+var mLab = "mongodb://localhost:27017/url-shortener-microservice";
+var MongoClient = mongodb.MongoClient
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/new/:url(*)', function (req, res, next) {
+  MongoClient.connect(mLab, function (err, db) {
+  if (err) {
+    console.log("Unable to connect to server", err);
+  } else {
+    console.log("Connected to server")
+    };
+  });
 });
 
 module.exports = router;
